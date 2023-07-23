d087cf43cf7625f40c130c2c99a8588efd8b07f9
Cheshta1828 <83445836+Cheshta1828@users.noreply.github.com>
2023-07-24 01:51:51 +0530

    final commit

---
+++ music-list.js
@@ -0,0 +1,41 @@
+
+
+let allMusic = [
+    {
+      name: "Its You",
+      artist: "Ali Gatie",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Perfect",
+      artist: "Ed Sheeran",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Saada Pyaar",
+      artist: "A.P Dhillon",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Gallan Mithiyan",
+      artist: "Mankirat Aulakh",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Yellow",
+      artist: "Cold Play",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "NGL",
+      artist: "KR$NA",
+      img: "music-6",
+      src: "music-6"
+    },
+    
+  ];
